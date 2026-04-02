9ff292c59ed352f69f280d98d2bcd572e0457c6a
hoho <riphoho12344@gmail.com>
2026-04-02 15:30:41 +0800

    Add basic port scanner functionality in main.py
    Implement a port scanner that checks for open ports on a specified IP address and optionally runs nmap for further verification.

---
+++ main.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+import os
+x = []
+g = input('which ip you want(ip v4): ') 
+if len(g) > 1:
+    pass
+else:
+     exit()
+try:
+    m = int(input('port 0-?(default 0-3000): '))
+except:
+    m = 3000 
+def a(port, g):
+        try:
+            s = socket.socket(socket.AF_INET,socket.SOCK_STREAM,0) 
+            s.settimeout(0.5)
+            s.connect((g,port))
+            print(port,' is open')
+            x.append(port)
+        except:
+            pass
+for i in range(1,m):
+    l = threading.Thread(target=a,args=(i,g))
+    l.start()
+
+r = input('want nmap for more sure serve?(y/n)')
+l = ''
+if(r == 'y'):
+    for i in x:
+        l = l + str(i) + ','    
+    os.system('nmap '+ g +' -sV -p '+l)
+else: 
+     print(x)
